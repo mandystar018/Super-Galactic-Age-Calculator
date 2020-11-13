a73104e3dca1c36e27a5535808c39ca3477bdd6b
Amanda Otero <mandystar018@gmail.com>
2020-11-13 09:52:06 -0800

    purposefully fail user constructor

---
+++ _tests_/user.test.js
@@ -0,0 +1,15 @@
+import User from '../src/js/user.js';
+
+  describe('User', () => {
+    let user;
+    beforeEach(() => {
+      user = new User("19", "active", "healthy");
+    });
+
+
+    test('It should correctly hold users data base on years, exercise, eatingHabits', () => {
+      expect(user.years).toEqual("19");
+      expect(user.exercise).toEqual("active");
+      expect(user.eatingHabits).toEqual("healthy");
+  });
+});
---
+++ src/js/user.js
@@ -0,0 +1,5 @@
+export class User {
+  constructor(years, exercise, eatingHabits) {
+    
+  }
+}
