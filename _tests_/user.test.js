05584c227f243c0f79f7da4154b1a9eddff4c096
Amanda Otero <mandystar018@gmail.com>
2020-11-13 10:07:32 -0800

    purposefully fail user total for each plnet

--- _tests_/user.test.js
+++ _tests_/user.test.js
@@ -12,4 +12,10 @@ import { User } from '../src/js/user.js';
       expect(user.exercise).toEqual("active");
       expect(user.eatingHabits).toEqual("healthy");
   });
+
+    test('It should add to the user total of 0 for each planet.', () => {
+      expect(user.venus).toEqual(0);
+      expect(user.mars).toEqual(0);
+      expect(user.jupite).toEqual(0);
+    })
 });
