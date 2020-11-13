2eeef4053930bbb8a60d0af20c3969225cc5cf27
Amanda Otero <mandystar018@gmail.com>
2020-11-13 14:36:46 -0800

    yearsMercury purposefully pass

--- _tests_/user.test.js
+++ _tests_/user.test.js
@@ -21,7 +21,7 @@ import { User } from '../src/js/user.js';
       });
 
     test('It should take the users year and divided by .24, round it and add it to mercury total', () => {
-      expect(user.yearsMercuy()).toEqual(79);
+      expect(user.yearsMercury()).toEqual(79);
     });
 
     // test('It should take the users year and divided by .24 and based on user exercide and eatingHabits is going to subtract from the mercury total', () => {
